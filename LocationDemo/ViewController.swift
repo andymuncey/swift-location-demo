d848c6331d2aaae4bb7efe69cf078588daea9bf8
Andrew Muncey <andrewmuncey@andrews-macbook-pro-2013.local>
2019-01-20 22:44:43 +0000

    Initial Commit

---
+++ LocationDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocationDemo
+//
+//  Created by Andrew Muncey on 20/01/2019.
+//  Copyright © 2019 Andrew Muncey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
